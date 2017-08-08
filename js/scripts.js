9ff80c2108f19958999095903b06de3d889fb893
Austin Cummings <skwerl_e@yahoo.com>
2017-08-08 00:51:49 -0700

    rewrote all of the world

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,21 @@ var sentence = prompt("Please enter a sentence.");
 console.log(sentence);
 
 
-var firstLetter=sentence.charAt(0);
-var lastLetter=sentence.charAt(sentence.length -1);
-var firstAndLast=firstLetter.concat(lastLetter);
-var capitalizedFirstAndLast=firstAndLast.toUpperCase();
-//alert(capitalizedFirstAndLast);
+var strInput = prompt("Please enter a sentence.");
+console.log(strInput);
+
+function getFirstLetter(sentence){
+return sentence[0]; //this is calling char array value of sentence
+};
+
+function getLastLetter(sentence){return sentence[sentence.length-1]};
+
+function getMiddleLetter(sentence){return sentence[Math.floor((sentence.length-1)/2)]}
+
+function reverseIt(sentence){return sentence.split("").reverse().join("")}
+alert(reverseIt(strInput))
+
+function capitalizedFirstAndLast(sentence){
+  return getFirstLetter(sentence).concat(getLastLetter(sentence)).toUpperCase();
+}
+alert(capitalizedFirstAndLast(strInput));
